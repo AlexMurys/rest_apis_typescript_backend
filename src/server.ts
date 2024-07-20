9d057a17b5420278d284719aea8aa213303f1b76
AlexMurys <skatebcn@gmail.com>
2024-07-20 19:39:14 +0200

    new data base has been added

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import express from "express";
 import router from "./router";
 import db from "./config/db";
 import colors from "colors";
-import cors, {CorsOptions} from 'cors'
-import morgan from 'morgan'
+import cors, { CorsOptions } from "cors";
+import morgan from "morgan";
 import swaggerUI from "swagger-ui-express";
 import swaggerSpec, { swaggerUIOptions } from "./config/swagger";
 
@@ -13,7 +13,7 @@ export async function connectDB() {
   try {
     await db.authenticate();
     db.sync();
-    //npconsole.log(colors.blue("Conexión exitosa a la BD"));
+    //console.log(colors.blue("Conexión exitosa a la BD"));
   } catch (error) {
     //console.log(error);
     console.log(colors.bgRed.white.bold("Hubo un error al conectar con la BD"));
@@ -26,20 +26,23 @@ connectDB();
 const server = express();
 
 // Permitir conexiones
-const corsOptions:CorsOptions = {
-  origin: function(origin, callback){
-    if(origin === process.env.FRONTEND_URL){
-      callback(null, true)
-    }else{callback(new Error('Error de CORS'))}
-  }
-}
+const corsOptions: CorsOptions = {
+  origin: function (origin, callback) {
+    console.log(origin);
+    if (origin === process.env.FRONTEND_URL) {
+      callback(null, true);
+    } else {
+      callback(new Error("Error de CORS"));
+    }
+  },
+};
 
-server.use(cors(corsOptions))
+server.use(cors(corsOptions));
 
 // Leer datos de formulario
 server.use(express.json());
 
-server.use(morgan('dev'))
+server.use(morgan("dev"));
 server.use("/api/products", router);
 
 //Docs
